6126a4a615449c48f2bc18a89d2f2792707581f7
Jose Quinones <jquinones618@gmail.com>
2017-04-23 12:21:45 -0400

    Removed tables and replaced with div's.  Altered .css to update page formatting.

--- index.php
+++ index.php
@@ -9,68 +9,68 @@
 
 <body>
 <header><!-- Header, nav, & hero image -->
-    <div class="container">
-        <div id="logo_wrapper"> <a href="index.php" id="logo">Nuaria Media Group</a> </div>
-        <nav>
-            <ul>
-                <li><a href="#">live recording</a></li>
-                <li><a href="#">mix &amp; master</a></li>
-                <li><a href="#">web design</a></li>
-            </ul>
-        </nav>
-    </div>
     <img src="images/hero_music.png" id="hero_image" alt=""/><!-- Hero img -->
-    <div id="hero_article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim </div>
+    <div class="container">
+        <div id="top-navigation">
+            <div id="logo_wrapper"><a href="index.php" id="logo">Nuaria Media Group</a></div>
+            <nav>
+                <ul>
+                    <li><a href="#">live recording</a></li>
+                    <li><a href="#">mix &amp; master</a></li>
+                    <li><a href="#">web design</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="hero_article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim
+        </div>
+    </div>
 </header>
 <!-- Main content section 1 -->
 <div class="content">
     <div class="container">
-        <table>
-            <tbody>
-            <tr>
-                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</td>
-            </tr>
-            </tbody>
-        </table>
+        <div id="article-left">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.</p>
+        </div>
+        <!-- Live Recording div -->
+        <div id="live_recording">
+            <div><img src="images/retro-mic.png" width="182" height="165" alt=""/></div>
+            <!-- Live Recording caption -->
+            <div class="caption">Live Recording</div>
+        </div><!-- End Live Recording pic and caption div -->
     </div>
 </div><!-- End Main content section 1 -->
 <!-- Preview content section -->
 <div class="red-backdrop">
     <div class="container">
-        <div id="inner_preview">
-            <!-- Live Recording div -->
-            <div id="live_recording">
-                <div><img src="images/retro-mic.png" width="182" height="165" alt=""/></div>
-                <!-- Live Recording caption -->
-                <div class="caption">Live Recording</div>
-            </div><!-- End Live Recording pic and caption div -->
-            <!-- Mix & Master div -->
-            <div id="mix_master">
-                <div><img src="images/recording-studio.png" width="185" height="162" alt=""/></div>
-                <!-- Mix & Master caption -->
-                <div class="caption">Mix &amp; Master</div>
-            </div><!-- End Mix & Master and caption div -->
-            <!-- Web Design div -->
-            <div id="web_design">
-                <div><img src="images/web-design-img.png" width="182" height="165" alt=""></div>
-                <div class="caption">Web Design</div>
-            </div><!-- End Web Design and caption div -->
-        </div><!-- End inner_preview -->
+        <div id="mix_master">
+            <div><img src="images/recording-studio.png" width="185" height="162" alt=""/></div>
+            <!-- Mix & Master caption -->
+            <div class="caption">Mix &amp; Master</div>
+        </div><!-- End Mix & Master and caption div -->
+        <div id="article-right">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.</p>
+        </div>
     </div>
 </div><!-- End preview -->
 <div class="clear"></div>
 <!-- Main content section 2 -->
 <div class="content">
     <div class="container">
-        <table>
-            <tbody>
-            <tr>
-                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</td>
-            </tr>
-            </tbody>
-        </table>
+        <div id="article-left">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.</p>
+        </div>
+        <!-- Web Design div -->
+        <div id="web_design">
+            <div><img src="images/web-design-img.png" width="143" height="126" alt=""></div>
+            <div class="caption">Web Design</div>
+        </div><!-- End Web Design and caption div -->
     </div>
 </div><!-- End Main content section 2 -->
 <!-- Form section -->
@@ -102,6 +102,6 @@
     </div>
 </div>
 <div class="clear"></div>
-<footer> Nuaria Media Group&copy; | <a href="#">About Us</a> | <a href="#">Contact Us</a> </footer>
+<footer> Nuaria Media Group&copy; | <a href="#">About Us</a> | <a href="#">Contact Us</a></footer>
 </body>
 </html>
